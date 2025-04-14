933b012bcb10aa6398018c5c3b16e829472df92b
vsch <vsch.org@gmail.com>
2025-04-14 11:41:58 +0300

    add platforms table to migrations

--- db/migrations/20250414081747_create_table_users.sql
+++ db/migrations/20250414081747_create_table_users.sql
@@ -2,14 +2,25 @@
 -- +goose StatementBegin
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    tg_user_id INTEGER UNIQUE NOT NULL,
+    platform_id INTEGER,
+    external_id TEXT NOT NULL,
     lang_id INTEGER,
     radius INTEGER,
+    username TEXT NOT NULL,
+    first_name TEXT DEFAULT "",
+    last_name TEXT DEFAULT "",
+    language_code TEXT NOT NULL,
+    is_bot boolean DEFAULT false,
+    is_premium boolean DEFAULT false,
     created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (tg_user_id) REFERENCES telegram_users(tg_id) ON DELETE CASCADE
+    FOREIGN KEY (platform_id) REFERENCES platforms(tid)
+        ON DELETE NO ACTION
         ON UPDATE CASCADE,
     FOREIGN KEY (lang_id) REFERENCES languages(id)
 );
+
+CREATE UNIQUE INDEX idx_users_platform_external_id
+        ON users(platform_id, external_id);
 -- +goose StatementEnd
 
 -- +goose Down
