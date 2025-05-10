0fb3a246acaa811bf8d96884a083e9b277a43c8a
vsch <vsch.org@gmail.com>
2025-05-10 10:52:54 +0300

    add handling for long messages

--- internal/message/build_text/build_text.go
+++ internal/message/build_text/build_text.go
@@ -13,27 +13,31 @@ import (
 func BuildTextForSearchChurchesByRadius(userCoords model.Coordinates, churches *[]church.DtoResponse, radius int, langCode string) string {
 
 	printer := i18n.Printer(langCode)
-	html := printer.Sprintf("event.callback.churches_found_in_radius", radius/1000, len(*churches))
+
+	var builder strings.Builder
+	builder.WriteString(printer.Sprintf("event.callback.churches_found_in_radius", radius/1000, len(*churches)))
 
 	for i, church := range *churches {
-		html += fmt.Sprintf("<b>%d.</b> ", i+1)
-		html += buildChurchesText(userCoords, church, langCode)
+		builder.WriteString(fmt.Sprintf("<b>%d.</b> ", i+1))
+		builder.WriteString(buildChurchesText(userCoords, church, langCode))
 	}
 
-	return html
+	return builder.String()
 }
 
 func BuildTextForTopNNearestChurches(userCoords model.Coordinates, churches *[]church.DtoResponse, langCode string) string {
 
 	printer := i18n.Printer(langCode)
-	html := printer.Sprintf("event.callback.nearby_churches_title")
+
+	var builder strings.Builder
+	builder.WriteString(printer.Sprintf("event.callback.nearby_churches_title"))
 
 	for i, church := range *churches {
-		html += fmt.Sprintf("<b>%d.</b> ", i+1)
-		html += buildChurchesText(userCoords, church, langCode)
+		builder.WriteString(fmt.Sprintf("<b>%d.</b> ", i+1))
+		builder.WriteString(buildChurchesText(userCoords, church, langCode))
 	}
 
-	return html
+	return builder.String()
 }
 
 func buildChurchesText(userCoords model.Coordinates, church church.DtoResponse, langCode string) string {
@@ -46,14 +50,16 @@ func buildChurchesText(userCoords model.Coordinates, church church.DtoResponse, 
 		churchConfession = church.ConfessionEn
 	}
 
+	var builder strings.Builder
+
 	vschUrl := fmt.Sprintf("https://www.vsch.org/church/%s", church.Alias)
-	html := fmt.Sprintf("<a href=\"%s\"><b>%s</b></a> (%s) – <b>[%.2f км]</b> <a href=\"https://www.google.com/maps/dir/%v,%v/%v,%v\">", vschUrl, churchName, churchConfession, church.Distance/1000, userCoords.Latitude, userCoords.Longitude, church.Latitude, church.Longitude)
+	builder.WriteString(fmt.Sprintf("<a href=\"%s\"><b>%s</b></a> (%s) – <b>[%.2f км]</b> <a href=\"https://www.google.com/maps/dir/%v,%v/%v,%v\">", vschUrl, churchName, churchConfession, church.Distance/1000, userCoords.Latitude, userCoords.Longitude, church.Latitude, church.Longitude))
 
 	printer := i18n.Printer(langCode)
-	html += printer.Sprintf("event.callback.route")
-	html += "</a>\n"
+	builder.WriteString(printer.Sprintf("event.callback.route"))
+	builder.WriteString("</a>\n")
 
-	return html
+	return builder.String()
 }
 
 func ForCountryWithChurches(countries *[]country.DtoResponse, langCode string) string {
